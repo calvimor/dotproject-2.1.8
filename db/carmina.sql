7d0e6c9e0b44daf66b5e68b7b3ea4fe2c2f5eeea
Jean Lelarge <info@ppkl.net>
2014-09-03 07:04:00 -0400

    Adding Carmina framework optional files re: pear captcha and phpbrowscap

--- db/carmina.sql
+++ db/carmina.sql
@@ -8,7 +8,9 @@
 # Changed: Remove explicit mySQL table type which then allows for being able to use other
 #			database engines
 
-CREATE TABLE `%dbprefix%login_header` (
+DROP TABLE IF EXISTS `dotp_login_header`;
+
+CREATE TABLE `dotp_login_header` (
   `login_header_id` int(11) NOT NULL AUTO_INCREMENT,
   `session_id` varchar(128) COLLATE latin1_general_ci NOT NULL,
   `added_date` datetime DEFAULT NULL,
@@ -16,9 +18,11 @@ CREATE TABLE `%dbprefix%login_header` (
   `ip_address` varchar(15) COLLATE latin1_general_ci DEFAULT NULL,
   PRIMARY KEY (`login_header_id`),
   KEY `sessionid` (`session_id`)
-) ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE `%dbprefix%phpbrowscap` (
+DROP TABLE IF EXISTS `dotp_phpbrowscap`;
+
+CREATE TABLE `dotp_phpbrowscap` (
   `browser_id` int(11) NOT NULL AUTO_INCREMENT,
   `session_id` varchar(128) COLLATE latin1_general_ci DEFAULT NULL,
   `browser_name` mediumtext COLLATE latin1_general_ci,
@@ -61,43 +65,4 @@ CREATE TABLE `%dbprefix%phpbrowscap` (
   `aolversion` varchar(128) COLLATE latin1_general_ci DEFAULT NULL,
   PRIMARY KEY (`browser_id`),
   KEY `sessionid` (`session_id`)
-) ;
-
-ALTER TABLE `%dbprefix%user_access_log` (
-
-) ;
-
-CREATE TABLE `%dbprefix%user_access_log` (
-`user_access_log_id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`user_id` INT( 10 ) UNSIGNED NOT NULL ,
-`user_ip` VARCHAR( 15 ) NOT NULL ,
-`date_time_in` DATETIME DEFAULT '0000-00-00 00:00:00',
-`date_time_out` DATETIME DEFAULT '0000-00-00 00:00:00',
-`date_time_last_action` DATETIME DEFAULT '0000-00-00 00:00:00',
-PRIMARY KEY ( `user_access_log_id` )
-);
---
--- Table structure for table `dotp_user_access_log`
---
-
-DROP TABLE IF EXISTS `dotp_user_access_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `dotp_user_access_log` (
-  `user_access_log_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
- * `session_id` varchar(40) NOT NULL,
-  `user_ip` varchar(15) NOT NULL,
- * `last_page_url` varchar(128) NOT NULL,
- * `browser_language` varchar(128) DEFAULT NULL,
-  `date_time_in` datetime DEFAULT '0000-00-00 00:00:00',
-  `date_time_out` datetime DEFAULT '0000-00-00 00:00:00',
-  `date_time_last_action` datetime DEFAULT '0000-00-00 00:00:00',
- * `ismobiledevice` tinyint(1) DEFAULT NULL,
-  *`referer` varchar(128) DEFAULT NULL,
-  *`crawler` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`user_access_log_id`),
-  KEY `session_id` (`session_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22093 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
